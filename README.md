43f9267b0fd09afae690c2deb419491e2790987d
Nick Pruehs <dev@npruehs.de>
2013-11-06 12:28:04 +0100

    ADDED: Assignment 01 - Vector and direction types.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-SAE-StyleDesignPrinciples
-=========================
+Style & Design Principles - Assignment Solutions
+================================================
+
+This repository contains the solution to all assignments of the lecture Style &
+Design Principles taught at [SAE Institute Hamburg](http://hamburg.sae.edu/de/home).
+
+Additional information can be found at my personal homepage at
+http://www.npruehs.de/teaching/style-design-principles/.
---
+++ Source/StyleDesignPrinciples/Direction.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Direction.cs" company="Nick Pruehs">
+//   Copyright 2013 Nick Pruehs.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace StyleDesignPrinciples
+{
+    public enum Direction
+    {
+        None = 0, 
+
+        North = 1 << 0, 
+
+        South = 1 << 1, 
+
+        West = 1 << 2, 
+
+        East = 1 << 3, 
+
+        NorthWest = North | West, 
+
+        NorthEast = North | East, 
+
+        SouthWest = South | West, 
+
+        SouthEast = South | East
+    }
+}
